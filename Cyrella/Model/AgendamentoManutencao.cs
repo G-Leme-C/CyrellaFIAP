c4079829f96bed70a04b24e0454dc026c6bf8f1f
Gabriel L Calsada <gabriellcalsada@gmail.com>
2021-04-16 19:21:23 -0300

    Commit inicial.

---
+++ Cyrella/Model/AgendamentoManutencao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cyrella.Model
+{
+    public class AgendamentoManutencao
+    {
+        public int Id { get; set; }
+        public DateTime DtAberturaChamado { get; set; }
+        public DateTime DtAgendamento { get; set; }
+        public string ItemComDefeito { get; set; }
+        public string DescricaoDefeito { get; set; }
+        public string Comodo { get; set; }
+        public List<string> UrlsImagens { get; set; }
+        public AreaAssistencia AreaDeAssistencia { get; set; }
+        public bool Recorrencia { get; set; }
+
+
+        public enum AreaAssistencia
+        {
+            Eletrica,
+            Hidraulica,
+            Alvenaria,
+            Pintura,
+            Marcenaria
+        }
+    }
+}
